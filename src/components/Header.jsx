f024c0a330c33ad289415b031bfca87ae24996cd
nvvvrro <m.navarrocarrillo@gmail.com>
2021-08-13 14:32:55 -0400

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper teal lighten-4">
+        <span className="brand-logo center">Formulario Usuario - AlterCap</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
